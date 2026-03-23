983c66c224275af088c68dea0a6e705c866cf632
Zorth Thorch <jasper_goens@hotmail.com>
2026-03-24 00:34:07 +0100

    fix: blog post frontmatter and update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'zorth.eu';
-export const SITE_DESCRIPTION = 'The official blog of zorth.eu';
+export const SITE_DESCRIPTION = 'The official blog of zorth';
