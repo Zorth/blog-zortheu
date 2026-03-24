902c1be3531c47b43f17082833395685aabf3168
Zorth Thorch <jasper_goens@hotmail.com>
2026-03-24 01:00:57 +0100

    feat: add remark-wiki-link for Obsidian-style header links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,25 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
+import wikiLink from 'remark-wiki-link';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.zorth.eu',
 	output: 'static',
 	integrations: [mdx(), sitemap(), vercel()],
+	markdown: {
+		remarkPlugins: [
+			[
+				wikiLink,
+				{
+					aliasDivider: '|',
+					hrefTemplate: (permalink) => {
+						if (permalink.startsWith('#')) return permalink.toLowerCase().replace(/[ _]/g, '-');
+						return `/blog/${permalink}`;
+					},
+				},
+			],
+		],
+	},
 });
